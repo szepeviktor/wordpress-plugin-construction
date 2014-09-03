7d86cb230cb9315d71b1ba1659e7c267e039c2b8
Viktor Szépe <viktor@szepe.net>
2014-09-03 03:12:16 +0200

    block-shortcodes

--- mu-block-shortcodes/block-shortcodes.php
+++ mu-block-shortcodes/block-shortcodes.php
@@ -14,16 +14,23 @@ function o1_block_shortcode( $atts, $content = '' ) {
     return o1_tag( 'div', $atts, $content );
 }
 
-function o1_tag( $tag, $attributes = array(), $content = '' ) {
+function o1_tag( $tag, $attributes = array(), $content = null ) {
 
     if ( empty( $tag ) || ! is_array( $attributes ) )
         return '';
 
     foreach ( $attributes as $attribute => &$data ) {
-      $data = implode( ' ', (array) $data );
-      $data = $attribute . '="' . esc_attr( $data ) . '"';
+        if ( empty( $data ) || true === $data ) {
+            // empty attributes
+            $data = esc_attr( $attribute );
+        } else {
+            $data = implode( ' ', (array) $data );
+            $data = $attribute . '="' . esc_attr( $data ) . '"';
+        }
     }
 
     $attribute_string =  $attributes ? ' ' . implode( ' ', $attributes ) : '';
-    return wp_kses_post( '<' . $tag . $attribute_string . '>' . $content . '</' . $tag . '>' );
+    $html = '<' . $tag . $attribute_string;
+    $html .= is_null( $content ) ? ' />' : '>' . $content . '</' . $tag . '>';
+    return wp_kses_post( $html );
 }
