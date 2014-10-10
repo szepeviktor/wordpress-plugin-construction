71308649881b0b29a78a53422a2fe7641d0087ba
Viktor Szépe <viktor@szepe.net>
2014-10-10 23:30:42 +0200

    Bitbucket support

---
+++ github-link/README.md
@@ -0,0 +1,14 @@
+## GitHub Link
+
+Displays GitHub link on the **Plugins** page given there is a GitHub Plugin URI` plugin header.
+
+### GitHub headers
+
+- "GitHub Plugin URI" shown as a normal GitHub icon
+- "GitHub Branch" shown as text after the GitHub icon
+- "GitHub Access Token" (aka. secret repo) shown as an inverted GitHub icon
+
+### Bitbucket headers
+
+- "Bitbucket Plugin URI" shown as a Bitbucket logo
+- "Bitbucket Branch" shown as text after the GitHub icon
--- github-link/github-link.php
+++ github-link/github-link.php
@@ -15,13 +15,46 @@ add_filter( 'plugin_action_links', 'GHL_plugin_link', 10, 4 );
 function GHL_plugin_link( $actions, $plugin_file, $plugin_data, $context ) {
 
     if ( 'search' !== $context ) {
-        $uri = get_file_data( trailingslashit( WP_PLUGIN_DIR ) . $plugin_file, array( "GitHubURI" => "GitHub Plugin URI" ) );
+        $plugin_data = get_file_data( trailingslashit( WP_PLUGIN_DIR ) . $plugin_file, array(
+            "GitHubURI" => "GitHub Plugin URI",
+            "GitHubBranch" => "GitHub Branch",
+            "GitHubToken" => "GitHub Access Token",
+            "BitbucketURI" => "Bitbucket Plugin URI",
+            "BitbucketBranch" => "Bitbucket Branch"
+        ) );
+        $link_template = '<a href="%s" target="_blank"><img src="%s" style="vertical-align:-3px" height="16" width="16" alt="%s" />%s</a>';
 
-        if ( ! empty( $uri["GitHubURI"] ) ) {
+        if ( ! empty( $plugin_data["GitHubURI"] ) ) {
+            $icon = "icon/GitHub-Mark-32px.png";
+            $branch = '';
+
+            if ( ! empty( $plugin_data["GitHubToken"] ) )
+                $icon = 'icon/GitHub-Mark-Light-32px.png" style="vertical-align:-3px;background-color:black;border-radius:50%';
+            if ( ! empty( $plugin_data["GitHubBranch"] ) )
+                $branch = '/' . $plugin_data["GitHubBranch"];
+
             $actions['github'] = sprintf(
-                '<a href="%s" target="_blank"><img src="%s" style="vertical-align:-3px" height="16" width="16" alt="GitHub" /></a>',
-                $uri["GitHubURI"],
-                plugins_url( "icon/GitHub-Mark-32px.png", __FILE__ )
+                $link_template,
+                $plugin_data["GitHubURI"],
+                plugins_url( $icon, __FILE__ ),
+                "GitHub",
+                $branch
+            );
+        }
+
+        if ( ! empty( $plugin_data["BitbucketURI"] ) ) {
+            $icon = "icon/bitbucket_32_darkblue_atlassian.png";
+            $branch = '';
+
+            if ( ! empty( $plugin_data["BitbucketBranch"] ) )
+                $branch = '/' . $plugin_data["BitbucketBranch"];
+
+            $actions['bitbucket'] = sprintf(
+                $link_template,
+                $plugin_data["BitbucketURI"],
+                plugins_url( $icon, __FILE__ ),
+                "Bitbucket",
+                $branch
             );
         }
     }
