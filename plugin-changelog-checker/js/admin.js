58639d0dfe895ef98b24cf1323582117149b5082
Viktor Szépe <viktor@szepe.net>
2014-10-10 20:36:37 +0200

    plugin-changelog-checker UI

---
+++ plugin-changelog-checker/js/admin.js
@@ -0,0 +1,28 @@
+jQuery(function ($) {
+
+    $('#wpbody #the-list tr').each(function () {
+        var action_link = $(this).find('.plugin-title span.watch a'),
+            plugin_file = action_link.data('plugin-file');
+
+        if (!plugin_file) console.error('No plugin data for' + String($(this).prop('id')));
+
+        action_link.click(function () {
+            var postdata,
+                action_link = $(this);
+
+            postdata = {
+                action: "o1_plugin_changelog_watch",
+                _nonce: O1_PluginChangelog_nonce,
+                plugin: plugin_file
+            };
+            $.post(
+                ajaxurl,
+                postdata,
+                function (result) {
+                    action_link.html($.parseJSON(result));
+                }
+            );
+        });
+    });
+
+});
--- plugin-changelog-checker/plugin-changelog.php
+++ plugin-changelog-checker/plugin-changelog.php
@@ -10,10 +10,7 @@ class O1_Plugin_Changelog_Checker {
 
     private $validator_url = "https://wordpress.org/plugins/about/validator/";
     private $compare_lines = 20;
-    private $plugin;
     private $alert_address;
-    private $changelog_page_url;
-    private $svn_url;
 
     private function get_top_lines( $html ) {
 
@@ -31,28 +28,42 @@ class O1_Plugin_Changelog_Checker {
 
     public function __construct() {
 
-        //TODO: options
-        $this->plugin = "better-wp-security";
+        add_filter( 'plugin_action_links', array( $this, 'plugin_link' ), 10, 4 );
+        add_action( 'admin_enqueue_scripts', array( $this, 'plugin_script' ) );
+        add_action( 'o1_daily_changelog_check', array( $this, 'check_changelogs' ) );
+        add_action( 'wp_ajax_o1_plugin_changelog_watch', array( $this, 'watch_ajax' ) );
 
-        add_action( 'o1_daily_changelog_check', array( $this, 'check_changelog' ) );
         register_deactivation_hook( __FILE__, array( $this, 'deactivation' ) );
     }
 
-    public function check_changelog() {
+    public function check_changelogs() {
 
         $this->alert_address = get_bloginfo( 'admin_email' );
-        $this->changelog_page_url = sprintf(
+        $watching = get_option( 'o1_plugin_changelog' );
+        if ( ! $watching )
+            $watching = array();
+
+        foreach ( $watching as $plugin ) {
+            if ( strpos( $plugin, '/' ) )
+                $plugin = dirname( $plugin );
+            $this->check_changelog( $plugin );
+        }
+    }
+
+    private function check_changelog( $plugin ) {
+
+        $changelog_page_url = sprintf(
             'https://wordpress.org/plugins/%s/changelog/',
-            $this->plugin
+            $plugin
         );
-        $this->svn_url = sprintf(
+        $svn_url = sprintf(
             'plugins.svn.wordpress.org/%s/trunk/readme.txt',
-            $this->plugin
+            $plugin
         );
         $http_args = array( 'sslverify' => false );
 
         // the Changelog page
-        $changelog_page = wp_remote_get( $this->changelog_page_url, $http_args );
+        $changelog_page = wp_remote_get( $changelog_page_url, $http_args );
         //if ( is_wp_error( $changelog_page ) ) { $response->get_error_message()
         $changelog_page_html = wp_remote_retrieve_body( $changelog_page );
         // if empty(
@@ -61,7 +72,7 @@ class O1_Plugin_Changelog_Checker {
         $changelog_page_10 = $this->get_top_lines( $changelog_page_top[1] );
 
         // readme.txt from trunk parsed with Readme Validator
-        $post_args = array( 'body' => array( 'url' => '1', 'readme_url' => $this->svn_url ) );
+        $post_args = array( 'body' => array( 'url' => '1', 'readme_url' => $svn_url ) );
         $svn = wp_remote_post( $this->validator_url, array_merge( $http_args, $post_args ) );
         //if ( is_wp_error( $changelog_page ) ) { $response->get_error_message()
         $svn_html = wp_remote_retrieve_body( $svn );
@@ -78,14 +89,65 @@ class O1_Plugin_Changelog_Checker {
             'SVN first line: %s' . "\n" . 'Changelog page first line: %s' . "\n" . '%s',
             serialize($svn_10[0]),
             serialize($changelog_page_10[0]),
-            $this->changelog_page_url
+            $changelog_page_url
         );
         $subject = sprintf(
             '[%s] Changelog mismatch',
-            $this->plugin
+            $plugin
         );
         wp_mail( $this->alert_address, $subject, $message );
         // if false ===
+    }
+
+    public function plugin_link( $actions, $plugin_file, $plugin_data, $context ) {
+
+        if ( 'search' !== $context ) {
+            $watching = get_option( 'o1_plugin_changelog' );
+            if ( ! $watching )
+                $watching = array();
+            $watch = in_array( $plugin_file, $watching );
+            $action = $watch ? 'Unwatch' : '<span style="color:green">Watch</span>';
+            $actions['watch'] = sprintf( '<a data-plugin-file="%s" href="#">%s</a>', $plugin_file, $action );
+        }
+        return $actions;
+    }
+
+    public function watch_ajax() {
+
+        check_ajax_referer( 'o1_plugin_changelog', '_nonce' );
+
+        $plugin_file = sanitize_text_field( $_POST['plugin'] );
+        $watching = get_option( 'o1_plugin_changelog' );
+        if ( ! $watching )
+            $watching = array();
+        $watch = in_array( $plugin_file, $watching );
+        $color = $watch ? '' : ' ';
+
+        if ( $watch ) {
+            // remove
+            $action = '<span style="color:green">Watch</span>';
+            $key = array_search($plugin_file, $watching);
+            unset( $watching[$key] );
+        } else {
+            // add
+            $action = 'Unwatch';
+            $watching[] = $plugin_file;
+        }
+
+        update_option( 'o1_plugin_changelog', $watching );
+        wp_die( json_encode( $action ) );
+
+    }
+
+    public function plugin_script( $hook ) {
+
+        // only in Plugins page
+        if ( 'plugins.php' !== $hook )
+            return;
+
+        wp_enqueue_script( 'o1_plugin_changelog_admin', plugin_dir_url( __FILE__ ) . 'js/admin.js', array( 'jquery' ) );
+        $nonce = wp_create_nonce( 'o1_plugin_changelog' );
+        wp_localize_script( 'o1_plugin_changelog_admin', 'O1_PluginChangelog_nonce', $nonce );
     }
 
     static function activation() {
