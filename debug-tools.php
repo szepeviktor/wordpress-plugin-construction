dc6899536d10bed0fc4eba3e326024b876863fae
Viktor Szépe <viktor@szepe.net>
2015-10-23 17:31:52 +0200

    Debug tools PHP

---
+++ debug-tools.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Snippet Name: Code blocks for debugging
+Version: 0.1.0
+Description: Use these blocks to find errors in PHP code.
+Snippet URI: https://github.com/szepeviktor/wordpress-plugin-construction
+*/
+
+/*
+Trace PHP-FPM
+    strace -s 200 -p $(pgrep -u root php5-fpm) -f 2>&1 | tee php-fpm.trc
+*/
+
+
+// Is it executed?
+echo "<!-- MARK -|-|-|-|- {$var} -->" . PHP_EOL; // FIXME
+
+
+// Is it executed?
+error_log( " -- MARK -- ". serialize( $var ) ); // FIXME
+
+
+// What files were executed?
+register_shutdown_function( function () {
+    echo '<!-- ' . PHP_EOL;
+    foreach ( get_included_files() as $i => $path ) {
+        printf( '%04d: %s%s', $i, $path, PHP_EOL );
+    }
+    echo ' -->' . PHP_EOL;
+    ob_flush();
+}); // FIXME
+
+
+// How does this called from?
+echo PHP_EOL . 'Trace: '; var_dump( debug_backtrace() ); exit; // FIXME
