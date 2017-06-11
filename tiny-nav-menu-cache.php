42ed96dfd87ad14938b6258b1f8fda5eca370083
Viktor Szépe <viktor@szepe.net>
2017-06-11 17:20:25 +0000

    Fix tiny-nav-menu-cache.php

--- tiny-nav-menu-cache.php
+++ tiny-nav-menu-cache.php
@@ -2,12 +2,8 @@
 /*
 Plugin Name: Tiny navigation menu cache (MU)
 Description: Cache nav menu's HTML content in persistent object cache.
-Version: 0.1.0
-
-->  https://wordpress.org/support/plugin/wp-nav-menu-cache
-    https://github.com/szepeviktor/w3-total-cache-fixed/issues/468
-    https://github.com/voceconnect/voce-cached-nav/blob/master/voce-cached-nav.php
-exclude by $args->theme_location ?
+Version: 0.1.1
+Constants: TINY_CACHE_NAV_MENU_EXCLUDES
 */
 
 class Tiny_Nav_Menu_Cache {
@@ -21,6 +17,12 @@ class Tiny_Nav_Menu_Cache {
 
     public function init() {
 
+        add_action( 'save_post', array( $this, 'flush_all' ) );
+        add_action( 'wp_create_nav_menu', array( $this, 'flush_all' ) );
+        add_action( 'wp_update_nav_menu', array( $this, 'flush_all' ) );
+        add_action( 'wp_delete_nav_menu', array( $this, 'flush_all' ) );
+        add_action( 'split_shared_term', array( $this, 'flush_all' ) );
+
         // Learned from W3TC Page Cache rules and WP Super Cache rules
         if ( ! wp_using_ext_object_cache() // Object cache is unavailable
             || is_user_logged_in() // User is logged in
@@ -32,22 +34,19 @@ class Tiny_Nav_Menu_Cache {
 
         add_filter( 'pre_wp_nav_menu', array( $this, 'get_nav_menu' ), 30, 2 );
         add_filter( 'wp_nav_menu', array( $this, 'save_nav_menu' ), PHP_INT_MAX, 2 );
-
-        add_action( 'save_post', array( $this, 'flush_all' ) );
-        add_action( 'wp_create_nav_menu', array( $this, 'flush_all' ) );
-        add_action( 'wp_update_nav_menu', array( $this, 'flush_all' ) );
-        add_action( 'wp_delete_nav_menu', array( $this, 'flush_all' ) );
-        add_action( 'split_shared_term', array( $this, 'flush_all' ) );
     }
 
     public function get_nav_menu( $nav_menu, $args ) {
 
         $key = $this->get_cache_key( $args );
-        $found = null;
-        $cache = wp_cache_get( $key, self::GROUP, false, $found );
-        if ( $found ) {
+        // Check excluded nav menus
+        if ( false !== $key ) {
+            $found = null;
+            $cache = wp_cache_get( $key, self::GROUP, false, $found );
+            if ( $found ) {
 
-            return $cache;
+                return $cache;
+            }
         }
 
         return $nav_menu;
@@ -56,8 +55,11 @@ class Tiny_Nav_Menu_Cache {
     public function save_nav_menu( $nav_menu, $args ) {
 
         $key = $this->get_cache_key( $args );
-        wp_cache_set( $key, $nav_menu, self::GROUP, DAY_IN_SECONDS );
-        $this->remember_key( $key );
+        // Check excluded nav menus
+        if ( false !== $key ) {
+            wp_cache_set( $key, $nav_menu, self::GROUP, DAY_IN_SECONDS );
+            $this->remember_key( $key );
+        }
 
         return $nav_menu;
     }
@@ -95,6 +97,18 @@ class Tiny_Nav_Menu_Cache {
     }
 
     private function get_cache_key( $args ) {
+
+        // Excluded theme locations
+        if ( defined( 'TINY_CACHE_NAV_MENU_EXCLUDES' ) && TINY_CACHE_NAV_MENU_EXCLUDES ) {
+            $excludes = explode( '|', TINY_CACHE_NAV_MENU_EXCLUDES );
+
+            if ( property_exists( $args, 'theme_location' )
+                && ! empty( $args->theme_location )
+                && in_array( $args->theme_location, $excludes )
+            ) {
+                return false;
+            }
+        }
 
         return md5( serialize( $args ) . $_SERVER['REQUEST_URI'] );
     }
