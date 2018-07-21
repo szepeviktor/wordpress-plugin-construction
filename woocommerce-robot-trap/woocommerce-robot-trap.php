2eebf60426017e39d66217c33a4bb6e4b7ed711a
Viktor Szépe <viktor@szepe.net>
2018-07-21 07:11:29 +0000

    Fix woocommerce-robot-trap processing

--- woocommerce-robot-trap/woocommerce-robot-trap.php
+++ woocommerce-robot-trap/woocommerce-robot-trap.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WooCommerce product reviews robot trap
-Version: 0.1.0
+Version: 0.1.1
 Description: Stops spammer robots, hide the field with CSS <code>.comment-form .email-verify { display:none; }</code>
 Plugin URI: https://github.com/szepeviktor/wordpress-plugin-construction
 License: The MIT License (MIT)
@@ -9,10 +9,10 @@ Author: Viktor Szépe
 GitHub Plugin URI: https://github.com/szepeviktor/wordpress-plugin-construction
 */
 
-add_filter( 'woocommerce_product_review_comment_form_args', 'wcprrt_print_comment_rating_hidden_field', 99 );
-add_action( 'comment_post', 'wcprrt_add_comment_rating_hidden_field', 0 );
+add_filter( 'woocommerce_product_review_comment_form_args', 'wcprrt_print_hidden_field', 99 );
+add_filter( 'preprocess_comment', 'wcprrt_check_hidden_field', 0 );
 
-function wcprrt_print_comment_rating_hidden_field( $comment_form ) {
+function wcprrt_print_hidden_field( $comment_form ) {
 
     $hidden_field = '<p class="email-verify"><label for="comment-email-verify">Email verify <span class="required">*</span></label><input id="comment-email-verify" name="comment-email-verify" type="text" value="" size="30" /></p>';
     // Prepend
@@ -21,15 +21,14 @@ function wcprrt_print_comment_rating_hidden_field( $comment_form ) {
     return $comment_form;
 }
 
-function wcprrt_add_comment_rating_hidden_field() {
+function wcprrt_check_hidden_field( $commentdata ) {
 
     $name = 'comment-email-verify';
-    if ( empty( $_POST[ $name ] ) ) {
-
-        return;
+    if ( ! empty( $_POST[ $name ] ) ) {
+        // Trigger firewall
+        $value = sanitize_text_field( $_POST[ $name ] );
+        do_action( 'robottrap_hiddenfield', $value );
     }
 
-    // Trigger firewall
-    $value = sanitize_text_field( $_POST[ $name ] );
-    do_action( 'robottrap_hiddenfield', $value );
+    return $commentdata;
 }
